4192a82466564b61fde57cc9d31692ed678158a7
joaomatosq <joaomatosq@gmail.com>
2025-06-04 17:25:14 -0300

    Adicionando cards e texto a seção de Problema

---
+++ js/script.js
@@ -0,0 +1,13 @@
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('apareceu');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  document.querySelectorAll('.card-animado').forEach(card => {
+    observer.observe(card);
+  });
