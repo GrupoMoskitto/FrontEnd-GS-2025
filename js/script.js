45b9c7b37d498792426d9eefc1922961ee9e54d4
joaomatosq <joaomatosq@gmail.com>
2025-06-04 17:26:53 -0300

    Criando animações dos cards usando javascript

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-  const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('apareceu');
       }
     });
-  }, {
+  }, 
+  
+  {
     threshold: 0.1
-  });
+ });
 
   document.querySelectorAll('.card-animado').forEach(card => {
-    observer.observe(card);
-  });
+  observer.observe(card);
+});
